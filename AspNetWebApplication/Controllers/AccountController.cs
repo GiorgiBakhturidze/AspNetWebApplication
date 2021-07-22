e929052cb09fadce00526d69dbc4df5e35d31a41
giorgibakhturidze2019 <giorgibakhturidze2019@outlook.com>
2021-07-21 20:50:22 -0700

    Add project files.

---
+++ AspNetWebApplication/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;                                                            
+using System;                                                                              
+using System.Collections.Generic;                                                          
+using System.Linq;                                                                         
+using System.Threading.Tasks;                                                              
+using Data.ApplicationModels;                                                              
+using Microsoft.AspNetCore.Identity;                                                       
+using Microsoft.AspNetCore.Authorization;                                                  
+                                                                                           
+namespace MVC.Controllers                                                                  
+{                                                                                          
+    [AllowAnonymous]                                                                       
+    public class AccountController : Controller                                            
+    {                                                                                      
+        private readonly SignInManager<ApplicationUser> _signInManager;                    
+        private readonly UserManager<ApplicationUser> _userManager;                        
+                                                                                           
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {                                                                                  
+            _signInManager = signInManager;                                                
+            _userManager = userManager;                                                    
+        }                                                                                  
+                                                                                           
+        [HttpGet]                                                                          
+        public IActionResult Login()                                                       
+        {                                                                                  
+            return View(new LoginInputModel());                                      
+        }                                                                                  
+                                                                                           
+        [HttpPost]                                                                         
+        public IActionResult Login(LoginInputModel inputedModel)                           
+        {                                                                                  
+            return View();                                                                 
+        }                                                                                  
+                                                                                           
+        [HttpGet]                                                                          
+        public ActionResult Register()                                                     
+        {                                                                                  
+            return View(new RegisterInputModel());                                 
+        }                                                                                  
+                                                                                           
+        [HttpPost]                                                                         
+        public async Task<IActionResult> Register(RegisterInputModel inputedModel)         
+        {                                                                                  
+            if (ModelState.IsValid && inputedModel.Password == inputedModel.ConfirmPassword)
+            {                                                                              
+                ApplicationUser newUser = new ApplicationUser { Email = inputedModel.Email, EmailConfirmed = true, PhoneNumberConfirmed = true, TwoFactorEnabled = false, LockoutEnabled = false, AccessFailedCount = 0 };
+                var result = await _userManager.CreateAsync(newUser, inputedModel.Password);
+                if (result.Succeeded)                                                      
+                {                                                                          
+                    await _signInManager.SignInAsync(newUser, isPersistent: false);        
+                }                                                                          
+            }                                                                              
+            return View();                                                                 
+        }                                                                                  
+    }                                                                                      
+}                                                                                          
+                                                                                           
---
+++ Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Data.ApplicationModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace Data
+{
+    public class DataContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-B3Q553I;Initial Catalog=WebAppDatabase;Integrated Security=True");
+        }
+    }
+}
